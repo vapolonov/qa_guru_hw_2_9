c687892787e625cf00f2e6b2e507338b4923f82f
AndrewVasutenko <vasutenko@gmail.com>
2022-12-07 21:28:45 +0200

    panic commit

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,21 +1,34 @@
-
 import pytest
-from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-
+from selene import Browser, Config
+import os
 from utils import attach
+from dotenv import load_dotenv
 
-@pytest.fixture(scope='function', autouse=True)
-def setup_browser():
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
+DEFAULT_BROWSER_VERSION = "100.0"
 
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -23,13 +36,16 @@ def setup_browser():
     }
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser.config.driver = driver
+    browser = Browser(Config(driver))
 
-    yield
+    yield browser
 
     attach.add_html(browser)
     attach.add_screenshot(browser)
--- Tests/test_demoqa_practice_form.py
+++ Tests/test_demoqa_practice_form.py
@@ -5,7 +5,7 @@ from Models import pages
 from selene.support.conditions import have
 
 @allure.title("Successful fill form")
-def test_demo_qa_form(setup_browser):
+def test_demo_qa_form():
 
     with allure.step("start demoqa"):
         browser.open("https://demoqa.com/automation-practice-form")
@@ -15,12 +15,12 @@ def test_demo_qa_form(setup_browser):
         pages.fill_user_email("qqqqq@qqqq.qq")
         controls.select_gender_male()
         pages.fill_user_number_phone("8800555353")
-        controls.fill_date_of_birthday("19 April,1993")
+        controls.fill_date_of_birthday("07 December,2022")
         pages.fill_subjects(("Art", "Commerce"))
         controls.select_hobby_music()
         controls.download_picture()
         pages.fill_curent_adress("Russia,Moscow")
-        controls.select_state_and_city("rajasthan", "jaipur")
+        controls.select_state_and_city("Rajasthan", "Jaipur")
         controls.submit()
 
     with allure.step("check result"):
@@ -29,7 +29,7 @@ def test_demo_qa_form(setup_browser):
         pages.table_result.should(have.text("qqqqq@qqqq.qq"))
         pages.table_result.should(have.text("Male"))
         pages.table_result.should(have.text("8800555353"))
-        pages.table_result.should(have.text("19 April,1993"))
+        pages.table_result.should(have.text("07 December,2022"))
         pages.table_result.should(have.text("Arts, Commerce"))
         pages.table_result.should(have.text("Music"))
         pages.table_result.should(have.text("meme.jpg"))
