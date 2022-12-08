758155e90c62cf9f83eb992f36ddfc186586eab0
AndrewVasutenko <vasutenko@gmail.com>
2022-12-08 20:01:25 +0200

    remove spaces

--- conftest.py
+++ conftest.py
@@ -1,20 +1,20 @@
+from selene.support.shared import browser
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-import os
 from utils import attach
 from dotenv import load_dotenv
-
-
+import os
 
 @pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
-
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def setup_browser():
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -26,20 +26,16 @@ def setup_browser():
     }
     options.capabilities.update(selenoid_capabilities)
 
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-
     driver = webdriver.Remote(
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver))
+    browser.config.driver = driver
 
-    yield browser
+    yield
 
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
-
     browser.quit()
