60b8217f0cf3a53de7df8f90ac6897d18fa0af92
AndrewVasutenko <vasutenko@gmail.com>
2022-12-08 19:10:44 +0200

    user1:1234

--- conftest.py
+++ conftest.py
@@ -40,7 +40,7 @@ def setup_browser(request):
     password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser = Browser(Config(driver))
