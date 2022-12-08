156169ec495d83762e42d5ad2ed713d2d6495ba1
AndrewVasutenko <vasutenko@gmail.com>
2022-12-08 20:05:28 +0200

    added login password

--- conftest.py
+++ conftest.py
@@ -26,6 +26,9 @@ def setup_browser():
     }
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
